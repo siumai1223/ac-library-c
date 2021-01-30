aa77ef347f47018e1fe7ce904fb884bbaf211f2d
siumai <57587646+siumai1223@users.noreply.github.com>
2021-01-30 13:03:14 +0900

    Update internal_graph.c
    ・関数の説明を追加
    ・辺のコストの型をtypedefで変えられるように変更
    ・↑にともなってgraph_initの引数を変更(data_sizeを削除)
    ・graph_pushの無向/有向オプションを削除

--- atcoder/internal_graph.c
+++ atcoder/internal_graph.c
@@ -1,8 +1,16 @@
 AOJ:Depth First Search　(http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B&lang=ja) での使用例　
-http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5179323#1
+http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5179430#1
 
-グラフ初期化時の引数は 頂点数,1(頂点ごとの隣接数だが、足りなくなる度に増やす　←この引数必要？)
-
+関数の説明
+graph_init(頂点数); グラフの初期化
+graph_push(グラフポインタ(?), 辺の始点, 辺の終点, 辺のコスト); グラフに辺を追加, 無向辺を追加する際は始点終点を入れ替えて計2度pushする
+graph_to(ポインタ, 辺の始点, インデックス:i); 始点のi番目の隣接点を返す
+graph_cost(ポインタ, 辺の始点, インデックス:i); 始点とi番目の隣接点を結ぶ辺のコストを返す
+                        
+また、type100行目付近のtypeof_graphにおいてコストの型を指定してるので適宜変更してください。
+*そのため同時に型の違うグラフを持つことが出来ません(要検討)
+                        
+                        
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -89,106 +97,56 @@ vector *vector_init(int n, int data_size) {
 #pragma endregion vector
 
 
-#pragma region vector2
-typedef struct vector2 vector2;
-
-typedef struct vector2{
-    int n, max, data_size;
-    vector **vec;
-    void (*push)(vector2*, int);
-} vector2;
-
-
-void vector2_push(vector2 *v2, int n){
-    if (v2->n == v2->max) {
-        v2->max *= 2;
-        assert((v2->vec = realloc(v2->vec, v2->max * sizeof(vector))) != NULL);
-        for(int i=v2->n; i<v2->max; i++){
-            v2->vec[i + 1] = vector_init(1, v2->data_size);
-        }
-    }
-    v2->n++;
-}
-
-vector2 *vector2_init(int n, int data_size) {
-    assert(n != 0);
-    vector2 *ret = (vector2 *)malloc(sizeof(vector2));
-    ret->n = 0;
-    ret->max = n;
-    ret->data_size = data_size;
-    ret->vec = (vector **)malloc(n * sizeof(vector*));
-    for(int i=0; i<n; i++){
-        (ret->vec[i]) = vector_init(1, sizeof(data_size));
-    }
-    ret->push = vector2_push;
-    return ret;
-}
-#pragma endregion vector2
-
-
 #pragma region graph
 
 typedef struct graph graph;
+
+typedef long long typeof_graph;
 
 typedef struct graph {
     int data_size;
     vector **vec_to;
     vector **vec_cost;
     int *ed_cnt;
-    void (*push)(graph*, int, int, long long, bool);
+    
+    void (*push)(graph*, int, int, typeof_graph);
     int (*to)(graph*, int, int);
-    long long (*cost)(graph*, int, int);
-    double (*cost_double)(graph*, int, int);
+    typeof_graph (*cost)(graph*, int, int);
 } graph;
 
 
-void graph_push(graph *g, int from, int to, long long cost, bool is_dir) {
+void graph_push(graph *g, int from, int to, typeof_graph cost) {
     g->vec_to[from]->push(g->vec_to[from], (void*)&to);
-    if(g->data_size != sizeof(bool)){
-        g->vec_cost[from]->push(g->vec_cost[from], (void*)&cost);
-    } 
+    g->vec_cost[from]->push(g->vec_cost[from], (void*)&cost);
     g->ed_cnt[from]++;
-
-    if(is_dir == false){
-        g->vec_to[to]->push(g->vec_to[to], (void*)&from);
-        if(g->data_size != sizeof(bool)){
-            g->vec_cost[to]->push(g->vec_cost[to], (void*)&cost);
-        } 
-        g->ed_cnt[to]++;
-    }
 }
 
 int graph_to(graph *g, int from, int idx){
     return g->vec_to[from]->get(g->vec_to[from], idx);
 }
-long long graph_cost(graph *g, int from, int idx){
+typeof_graph graph_cost(graph *g, int from, int idx){
     return g->vec_to[from]->get(g->vec_to[from], idx);
 }
-double graph_cost_double(graph *g, int from, int idx){
-    return g->vec_to[from]->get_double(g->vec_to[from], idx);
-}
 
-graph *graph_init(int n, int data_size) {
+graph *graph_init(int n) {
     graph *ret = (graph *)malloc(sizeof(graph));
-    ret->data_size = data_size;
+    ret->data_size = sizeof(typeof_graph);
+    ret->ed_cnt = (int *)malloc(n * sizeof(int));
+    
     ret->vec_to = (vector **)malloc(n * sizeof(vector*));
     for(int i=0; i<n; i++){
         (ret->vec_to[i]) = vector_init(1, sizeof(int));
     }
-    if(data_size != sizeof(bool)){
-        ret->vec_cost = (vector **)malloc(n * sizeof(vector*));
-        for(int i=0; i<n; i++){
-            (ret->vec_cost[i]) = vector_init(1, sizeof(data_size));
-        }
-    }
-    ret->ed_cnt = (int *)malloc(n * sizeof(int));
 
+    ret->vec_cost = (vector **)malloc(n * sizeof(vector*));
+    for(int i=0; i<n; i++){
+        (ret->vec_cost[i]) = vector_init(1, sizeof(typeof_graph));
+    }
+    
     ret->push = graph_push;
     ret->to = graph_to;
     ret->cost = graph_cost;
-    ret->cost_double = graph_cost_double;
     return ret;
 }
 
 #pragma endregion graph
-
